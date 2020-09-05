a73459247d1bb994035de987481b0db79da1bb72
prajjwal jaiswal <jaiswalprajjwal486@gmail.com>
2020-09-05 13:00:12 +0530

    'Initial'

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// Set template engine
+
+app.use(expressLayout);
+
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.set('view engine', 'ejs');
+
+app.listen(3000, () => {
+    console.log("Listining port "+PORT);
+});

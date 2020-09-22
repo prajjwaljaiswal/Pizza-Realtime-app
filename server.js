811ac9a9303538ad9d7202b7c8fb83630b135c0b
prajjwal jaiswal <jaiswalprajjwal486@gmail.com>
2020-09-22 22:44:46 +0530

    'DesignApp'

--- server.js
+++ server.js
@@ -7,12 +7,25 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Set template engine
+
+app.use(expressLayout);
+
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.set('view engine', 'ejs');
+
+
 // assets
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/cart', (req, res) =>  {
+    res.render('customers/cart');
 });
 
 
